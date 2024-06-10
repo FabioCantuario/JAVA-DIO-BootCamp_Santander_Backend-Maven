fe5988cb13c9e705d2f43f2f4ffc2604a2365037
Fabio Cantuario <fabiocantuario@hotmail.com>
2024-06-09 22:05:40 -0400

    Alteração no Projeto Maven com jUnit

--- src/test/java/br/com/teste/maven/maven/PessoaTeste.java
+++ src/test/java/br/com/teste/maven/maven/PessoaTeste.java
@@ -3,13 +3,23 @@ package br.com.teste.maven.maven;
 import java.time.LocalDateTime;
 
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class PessoaTeste {
 	
 	@Test
 	void deveCalcularIdadeCorreta() {
 		Pessoa fabio = new Pessoa("Fábio", LocalDateTime.of(2000,1,1,15,0,0));
-		//Assertions.assertEquals(22,fabio.getIdade());
+		Assertions.assertEquals(22,fabio.getIdade());
+	}
+	
+	@Test
+	void validarLancamento() {
+		int[] primeiro = { 10,20,30,50,40};
+		int[] segundo = {-1,20,30,40,50};
+		
+		Assertions.assertArrayEquals(primeiro,segundo);
 	}
 
 }
+
